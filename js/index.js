66852ee6f2f9f2b2c063f163f6db99f238d3024b
Arver1 <aactepanov@mail.ru>
2018-08-07 20:36:23 +0300

    before move field all devices

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 import { ENTER_KEY_CODE } from './util';
 
-const menu = document.querySelector('[for="burger_menu"]');
+const mainMenu = document.querySelector('[for="burger_menu"]');
 const menuControl = document.getElementById('burger_menu');
+const devicesMenu = document.querySelector('[for="all_devices"]');
+const devicesMenuControl = document.getElementById('all_devices');
 
-menu.addEventListener('keydown', (e) => {
-  if (e.keyCode === ENTER_KEY_CODE) {
-    menuControl.checked = !menuControl.checked;
-  }
-});
+addEventKeyOnMenu(mainMenu, menuControl);
+addEventKeyOnMenu(devicesMenu, devicesMenuControl);
+
+function addEventKeyOnMenu(label, input, keyCode = ENTER_KEY_CODE) {
+  label.addEventListener('keydown', (e) => {
+    if (e.keyCode === keyCode) {
+      input.checked = !input.checked;
+    }
+  });
+}
